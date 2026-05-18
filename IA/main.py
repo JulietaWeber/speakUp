b432ca7f86d7dcc9f907b665cf6b41dd2f6b5dcb
Maxinudel <166538999+Maxinudel@users.noreply.github.com>
2026-05-18 11:37:04 -0300

    sprint 1 atroden (aura)

--- IA/main.py
+++ IA/main.py
@@ -12,3 +12,39 @@ def registrar_palabra(palabra):
         frecuencia_palabras[palabra] += 1
     else:
         frecuencia_palabras[palabra] = 1
+
+
+def registrar_transicion(palabra_actual, palabra_siguiente):
+    if palabra_actual not in transiciones:
+        transiciones[palabra_actual] = {}
+
+    if palabra_siguiente in transiciones[palabra_actual]:
+        transiciones[palabra_actual][palabra_siguiente] += 1
+    else:
+        transiciones[palabra_actual][palabra_siguiente] = 1
+
+
+def procesar_frase(frase):
+    for i in range(len(frase)):
+        registrar_palabra(frase[i])
+
+        if i < len(frase) - 1:
+            registrar_transicion(frase[i], frase[i + 1])
+
+    historial_frases.append(frase)
+
+
+# Pruebas con datos simulados
+procesar_frase(["yo", "quiero", "agua"])
+procesar_frase(["yo", "quiero", "ir", "baño"])
+procesar_frase(["yo", "quiero", "comida"])
+
+
+print("Frecuencia de palabras:")
+print(frecuencia_palabras)
+
+print("\nTransiciones:")
+print(transiciones)
+
+print("\nHistorial de frases:")
+print(historial_frases)
