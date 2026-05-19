11744c1192de8dbcfe022deea6074e56c77e4e32
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2026-05-19 08:07:41 -0300

    conexión con la base de datos

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const supabase = require("backend/src/config/supabase.js");
+const supabase = require("./supabase");
 
 const app = express();
 
