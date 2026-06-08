31d9d202630b50ff79cfcbfc7c8676f2db228cf0
JulietaWeber <166619703+JulietaWeber@users.noreply.github.com>
2026-06-08 12:10:12 -0300

    VAMOSSSSS

--- frontend/src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "expo-router";
-
-//redirigir automáticamente al usuario a otra pantalla.
-
-export default function Index() {
-  return <Redirect href="/login" />;
-} 
-
-//Defino la pantalla inicial
