3704362a6772bf17b37152149196e13d50ce9a7b
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2026-07-13 15:23:01 -0300

    Changes (relación de id con token)

--- backend/src/routes/tableros.routes.js
+++ backend/src/routes/tableros.routes.js
@@ -1,23 +1,21 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
-    obtenerTableros,
-    crearTablero,
-    obtenerTableroPorId,
-    actualizarTablero,
-    eliminarTablero
-  } = require("../controllers/tableros.controller");
+  obtenerMisTableros,
+  crearTablero,
+  obtenerPictogramasDeTablero,
+  agregarPictogramaATablero,
+  eliminarPictogramaDeTablero
+} = require("../controllers/tableros.controller");
 
-router.get("/", obtenerTableros);
+const verificarToken = require("../middlewares/auth.middleware");
 
-router.post("/", crearTablero);
+router.get("/mis-tableros", verificarToken, obtenerMisTableros);
+router.post("/", verificarToken, crearTablero);
 
-router.get("/:id", obtenerTableroPorId);
-
-router.put("/:id", actualizarTablero);
-
-router.delete("/:id", eliminarTablero);
+router.get("/:id_tablero/pictogramas", verificarToken, obtenerPictogramasDeTablero);
+router.post("/:id_tablero/pictogramas", verificarToken, agregarPictogramaATablero);
+router.delete("/:id_tablero/pictogramas/:id_pictogramas", verificarToken, eliminarPictogramaDeTablero);
 
 module.exports = router;
