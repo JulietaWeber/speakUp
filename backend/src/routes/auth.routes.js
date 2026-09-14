684c71b24dc8bc543da9db3f620a12fb8064005d
Maytecalvert <166538878+Maytecalvert@users.noreply.github.com>
2026-09-14 11:33:19 -0300

    Probando con google, + auth

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,16 +1,22 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
   registrarUsuario,
   loginUsuario,
+  loginGoogle,
   obtenerMiPerfil
 } = require("../controllers/auth.controller");
 
 const verificarToken = require("../middlewares/auth.middleware");
 
 router.post("/register", registrarUsuario);
+
 router.post("/login", loginUsuario);
+
+router.post("/google", loginGoogle);
+
 router.get("/me", verificarToken, obtenerMiPerfil);
 
 module.exports = router;
